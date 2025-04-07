f6d44716c4a350f62afd8b4fb994fee4996510a3
PrajwalMadikai <133801108+PrajwalMadikai@users.noreply.github.com>
2025-04-07 17:06:48 +0530

    render changes

--- src/database/db.ts
+++ src/database/db.ts
@@ -9,6 +9,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: parseInt(process.env.DB_PORT || '5432', 10),
+  ssl: {
+    rejectUnauthorized: false 
+  }
 });
 
 pool.query('SELECT NOW()', (err, res) => {
